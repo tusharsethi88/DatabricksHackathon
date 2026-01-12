7a084d1f6239764618e7b7f7c406bfb448694985
Tushar Sethi <43496049+tusharsethi88@users.noreply.github.com>
2026-01-12 20:12:32 +0530

    Add files via upload

---
+++ schema.rb
@@ -0,0 +1,133 @@
+# This file is auto-generated from Unity Catalog metadata
+# Generated at: 2026-01-12T14:39:14.854951 UTC
+
+ActiveRecord::Schema.define(version: 0) do
+
+  # UC: hackathon_agile_minds.cdl.banking_clean  (type: MANAGED)
+  # UC: hackathon_agile_minds.cdl.banking_clean_demo  (type: MANAGED)
+  # UC: hackathon_agile_minds.cdl.banking_dq_issues_severe  (type: MANAGED)
+  # UC: hackathon_agile_minds.cdl.banking_dqissues_demo  (type: MANAGED)
+  # UC: hackathon_agile_minds.cdl.banking_prev_batch_severe  (type: MANAGED)
+  # UC: hackathon_agile_minds.information_schema.catalog_privileges  (type: VIEW)
+  # create_view :hackathon_agile_minds_information_schema_catalog_privileges  # views are documented here; definitions live in UC
+
+  # UC: hackathon_agile_minds.information_schema.catalog_tags  (type: VIEW)
+  # create_view :hackathon_agile_minds_information_schema_catalog_tags  # views are documented here; definitions live in UC
+
+  # UC: hackathon_agile_minds.information_schema.catalogs  (type: VIEW)
+  # create_view :hackathon_agile_minds_information_schema_catalogs  # views are documented here; definitions live in UC
+
+  # UC: hackathon_agile_minds.information_schema.check_constraints  (type: VIEW)
+  # create_view :hackathon_agile_minds_information_schema_check_constraints  # views are documented here; definitions live in UC
+
+  # UC: hackathon_agile_minds.information_schema.column_masks  (type: VIEW)
+  # create_view :hackathon_agile_minds_information_schema_column_masks  # views are documented here; definitions live in UC
+
+  # UC: hackathon_agile_minds.information_schema.column_tags  (type: VIEW)
+  # create_view :hackathon_agile_minds_information_schema_column_tags  # views are documented here; definitions live in UC
+
+  # UC: hackathon_agile_minds.information_schema.columns  (type: VIEW)
+  # create_view :hackathon_agile_minds_information_schema_columns  # views are documented here; definitions live in UC
+
+  # UC: hackathon_agile_minds.information_schema.constraint_column_usage  (type: VIEW)
+  # create_view :hackathon_agile_minds_information_schema_constraint_column_usage  # views are documented here; definitions live in UC
+
+  # UC: hackathon_agile_minds.information_schema.constraint_table_usage  (type: VIEW)
+  # create_view :hackathon_agile_minds_information_schema_constraint_table_usage  # views are documented here; definitions live in UC
+
+  # UC: hackathon_agile_minds.information_schema.information_schema_catalog_name  (type: VIEW)
+  # create_view :hackathon_agile_minds_information_schema_information_schema_catalog_name  # views are documented here; definitions live in UC
+
+  # UC: hackathon_agile_minds.information_schema.key_column_usage  (type: VIEW)
+  # create_view :hackathon_agile_minds_information_schema_key_column_usage  # views are documented here; definitions live in UC
+
+  # UC: hackathon_agile_minds.information_schema.parameters  (type: VIEW)
+  # create_view :hackathon_agile_minds_information_schema_parameters  # views are documented here; definitions live in UC
+
+  # UC: hackathon_agile_minds.information_schema.referential_constraints  (type: VIEW)
+  # create_view :hackathon_agile_minds_information_schema_referential_constraints  # views are documented here; definitions live in UC
+
+  # UC: hackathon_agile_minds.information_schema.routine_columns  (type: VIEW)
+  # create_view :hackathon_agile_minds_information_schema_routine_columns  # views are documented here; definitions live in UC
+
+  # UC: hackathon_agile_minds.information_schema.routine_privileges  (type: VIEW)
+  # create_view :hackathon_agile_minds_information_schema_routine_privileges  # views are documented here; definitions live in UC
+
+  # UC: hackathon_agile_minds.information_schema.routines  (type: VIEW)
+  # create_view :hackathon_agile_minds_information_schema_routines  # views are documented here; definitions live in UC
+
+  # UC: hackathon_agile_minds.information_schema.row_filters  (type: VIEW)
+  # create_view :hackathon_agile_minds_information_schema_row_filters  # views are documented here; definitions live in UC
+
+  # UC: hackathon_agile_minds.information_schema.schema_privileges  (type: VIEW)
+  # create_view :hackathon_agile_minds_information_schema_schema_privileges  # views are documented here; definitions live in UC
+
+  # UC: hackathon_agile_minds.information_schema.schema_tags  (type: VIEW)
+  # create_view :hackathon_agile_minds_information_schema_schema_tags  # views are documented here; definitions live in UC
+
+  # UC: hackathon_agile_minds.information_schema.schemata  (type: VIEW)
+  # create_view :hackathon_agile_minds_information_schema_schemata  # views are documented here; definitions live in UC
+
+  # UC: hackathon_agile_minds.information_schema.table_constraints  (type: VIEW)
+  # create_view :hackathon_agile_minds_information_schema_table_constraints  # views are documented here; definitions live in UC
+
+  # UC: hackathon_agile_minds.information_schema.table_privileges  (type: VIEW)
+  # create_view :hackathon_agile_minds_information_schema_table_privileges  # views are documented here; definitions live in UC
+
+  # UC: hackathon_agile_minds.information_schema.table_tags  (type: VIEW)
+  # create_view :hackathon_agile_minds_information_schema_table_tags  # views are documented here; definitions live in UC
+
+  # UC: hackathon_agile_minds.information_schema.tables  (type: VIEW)
+  # create_view :hackathon_agile_minds_information_schema_tables  # views are documented here; definitions live in UC
+
+  # UC: hackathon_agile_minds.information_schema.views  (type: VIEW)
+  # create_view :hackathon_agile_minds_information_schema_views  # views are documented here; definitions live in UC
+
+  # UC: hackathon_agile_minds.information_schema.volume_privileges  (type: VIEW)
+  # create_view :hackathon_agile_minds_information_schema_volume_privileges  # views are documented here; definitions live in UC
+
+  # UC: hackathon_agile_minds.information_schema.volume_tags  (type: VIEW)
+  # create_view :hackathon_agile_minds_information_schema_volume_tags  # views are documented here; definitions live in UC
+
+  # UC: hackathon_agile_minds.information_schema.volumes  (type: VIEW)
+  # create_view :hackathon_agile_minds_information_schema_volumes  # views are documented here; definitions live in UC
+
+  # UC: hackathon_agile_minds.odl.accountmaster  (type: MANAGED)
+  # UC: hackathon_agile_minds.odl.customermaster  (type: MANAGED)
+  # UC: hackathon_agile_minds.pdl.customermaster  (type: MANAGED)
+  # UC: hackathon_agile_minds.pdl.transaction  (type: MANAGED)
+  # UC: hackathon_agile_minds.source.banking_clean  (type: MANAGED)
+  # UC: hackathon_agile_minds.source.banking_clean_demo  (type: MANAGED)
+  # UC: hackathon_agile_minds.source.banking_dq_issues_severe  (type: MANAGED)
+  # UC: hackathon_agile_minds.source.banking_dqissues_demo  (type: MANAGED)
+  # UC: hackathon_agile_minds.source.banking_prev_batch_severe  (type: MANAGED)
+  # UC: hackathon_agile_minds.source_quality.all_anomaly_agg_table  (type: MANAGED)
+  # UC: hackathon_agile_minds.source_quality.all_anomaly_table  (type: MANAGED)
+  # UC: hackathon_agile_minds.source_quality.all_anomaly_table_correction  (type: MANAGED)
+  # UC: hackathon_agile_minds.source_quality.all_anomaly_table_correction_suggestion  (type: MANAGED)
+  # UC: hackathon_agile_minds.source_quality.all_databricks_job_run  (type: MANAGED)
+  # UC: hackathon_agile_minds.source_quality.all_daxexpressions  (type: MANAGED)
+  # UC: hackathon_agile_minds.source_quality.all_dqstatus  (type: MANAGED)
+  # UC: hackathon_agile_minds.source_quality.all_failed_agg_table  (type: MANAGED)
+  # UC: hackathon_agile_minds.source_quality.all_failed_table  (type: MANAGED)
+  # UC: hackathon_agile_minds.source_quality.all_failed_table_correction  (type: MANAGED)
+  # UC: hackathon_agile_minds.source_quality.all_failed_table_correction_suggestion  (type: MANAGED)
+  # UC: hackathon_agile_minds.source_quality.all_qualigencheck  (type: MANAGED)
+  # UC: hackathon_agile_minds.source_quality.banking_clean_demo_anomalyaggtbl  (type: MANAGED)
+  # UC: hackathon_agile_minds.source_quality.banking_clean_demo_anomalytbl  (type: MANAGED)
+  # UC: hackathon_agile_minds.source_quality.banking_clean_demo_failedaggtbl  (type: MANAGED)
+  # UC: hackathon_agile_minds.source_quality.banking_clean_demo_failedtbl  (type: MANAGED)
+  # UC: hackathon_agile_minds.source_quality.banking_dq_issues_severe_anomalyaggtbl  (type: MANAGED)
+  # UC: hackathon_agile_minds.source_quality.banking_dq_issues_severe_anomalytbl  (type: MANAGED)
+  # UC: hackathon_agile_minds.source_quality.banking_dq_issues_severe_failedaggtbl  (type: MANAGED)
+  # UC: hackathon_agile_minds.source_quality.banking_dq_issues_severe_failedtbl  (type: MANAGED)
+  # UC: hackathon_agile_minds.source_quality.banking_dqissues_demo_anomalyaggtbl  (type: MANAGED)
+  # UC: hackathon_agile_minds.source_quality.banking_dqissues_demo_anomalytbl  (type: MANAGED)
+  # UC: hackathon_agile_minds.source_quality.banking_dqissues_demo_failedaggtbl  (type: MANAGED)
+  # UC: hackathon_agile_minds.source_quality.banking_dqissues_demo_failedtbl  (type: MANAGED)
+  # UC: hackathon_agile_minds.source_quality.banking_prev_batch_severe_anomalyaggtbl  (type: MANAGED)
+  # UC: hackathon_agile_minds.source_quality.banking_prev_batch_severe_anomalytbl  (type: MANAGED)
+  # UC: hackathon_agile_minds.source_quality.banking_prev_batch_severe_failedaggtbl  (type: MANAGED)
+  # UC: hackathon_agile_minds.source_quality.banking_prev_batch_severe_failedtbl  (type: MANAGED)
+  # UC: hackathon_agile_minds.source_quality.databricks_job_run  (type: MANAGED)
+end
